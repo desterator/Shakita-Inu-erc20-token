a9714becb88d86feff5d20e04576282eb190d2fe
DzenkivV <viktor.dzenkiv@nure.ua>
2021-07-15 00:39:06 +0300

    implement deploy scripts

--- contracts/interface/IBEP20.sol
+++ contracts/interface/IBEP20.sol
@@ -1,6 +1,7 @@
-pragma solidity ^0.7.0;
+pragma solidity 0.8.6;
 
 interface IBEP20 {
+    function deposit() external payable;
     function balanceOf(address _account) external view returns(uint256);
     function approve(address _spender, uint256 _addedValue) external returns (bool);
 }
--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,25 +1,29 @@
 const Shakita = artifacts.require("Shakita");
 const Router = artifacts.require("./interface/test/IPancakeRouter");
-const Router = artifacts.require("./interface/test/IBEP20");
+const IBEP20 = artifacts.require("./interface/test/IBEP20");
 
 
-module.exports = async function (deployer) {
-  const ISSUER = "0xb89600B37fB1816416abD4625FDA029017f66B89";
+module.exports = async function (deployer, network, accounts) {
+  const ISSUER = accounts[0];
   const routerAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
 
-  await deployer.deploy(Shakita);
+  await deployer.deploy(Shakita, ISSUER);
   const shakita = await Shakita.deployed();
   const router = await Router.at(routerAddress);
   const weth = await IBEP20.at(await router.WETH());
-  await weth.approve(router.address);
-  shakita.approve(router.address);
+  await weth.approve(router.address, "100000000000010000000000001000000000000");
+  await weth.deposit({value: "90000000000000000000"});
+  await shakita.approve(router.address, "100000000000010000000000001000000000000");
   
+  
+  await shakita.changeSalesTax(); // todo do we need change it now????
+
   await router.addLiquidity(shakita.address,
                             weth.address,
-                            "1000000000000000000000",
-                            "1000000000000000000000",
-                            "1000000000000000000000",
-                            "1000000000000000000000",
+                            "90000000000000000000",
+                            "90000000000000000000",
+                            "90000000000000000000",
+                            "90000000000000000000",
                             ISSUER,
                             "100000000000010000000000001000000000000");
 };
