75c8a0f2e311aedf131ff69b75b0b9f8ddbd78ff
DzenkivV <viktor.dzenkiv@nure.ua>
2021-07-23 22:20:19 +0300

    add turn for anti whale

--- contracts/Shakita.sol
+++ contracts/Shakita.sol
@@ -8,6 +8,7 @@ contract Shakita is ERC20, Ownable {
     using SafeMath for uint256;
 
     bool public SalesTax;
+    bool public AtniWhale;
 
     address constant public GameRewards = 0x8e2665836732d07b028a99E04d8366dffBe5d6a5;
     address constant public NFTfarm = 0x4EeDF91ad3BAc5283C17Cb6fAba500aFBBfBAad8;
@@ -48,16 +49,16 @@ contract Shakita is ERC20, Ownable {
         SalesTax = !SalesTax;
     }
 
+    function changeAtniWhale() external onlyOwner {
+        AtniWhale = !AtniWhale;
+    }
+
     // make tax for transfers, make anti whale
     function _afterTokenTransfer(
         address from,
         address to,
         uint256 amount
     ) internal override virtual {
-        if (!SalesTax) {
-            return;
-        }
-
         // no tax and anti whale for whiteList
         if(whiteList[from]) {
             return;
@@ -69,30 +70,35 @@ contract Shakita is ERC20, Ownable {
         }
 
         // apply commission sanctions 10%
-        uint256 _percent = amount.div(100);
-        if (_percent == 0) {
-            return;
+        if (SalesTax) {
+            uint256 _percent = amount.div(100);
+            if (_percent == 0) {
+                return;
+            }
+
+            ERC20._mint(GameRewards, _percent);
+            ERC20._mint(NFTfarm, _percent.mul(2));
+            ERC20._mint(Marketing, _percent.mul(3));
+            // means sell on pancake router
+            if(sellList[to]) {
+                ERC20._mint(Burn, _percent.mul(4));
+            } else {
+                ERC20._mint(ShakitaFund, _percent.mul(4));
+            }
+            ERC20._burn(to, _percent.mul(10));
         }
 
-        ERC20._mint(GameRewards, _percent);
-        ERC20._mint(NFTfarm, _percent.mul(2));
-        ERC20._mint(Marketing, _percent.mul(3));
-        // means sell on pancake router
-        if(sellList[to]) {
-            ERC20._mint(Burn, _percent.mul(4));
-        } else {
-            ERC20._mint(ShakitaFund, _percent.mul(4));
-        }
-        ERC20._burn(to, _percent.mul(10));
-
-        // apply anti whale sanctions
         // because admins can have more then limit
         if(whiteList[to]) {
             return;
         }
-        // 300m
-        require(ERC20.balanceOf(to) <= 300000000000000000000000000, "balanceExceedsLimit");
-        // 30m
-        require(amount <= 30000000000000000000000000, "amountExceedsLimit");
+
+        // apply anti whale sanctions
+        if(AtniWhale) {
+            // 300m
+            require(ERC20.balanceOf(to) <= 300000000000000000000000000, "balanceExceedsLimit");
+            // 30m
+            require(amount <= 30000000000000000000000000, "amountExceedsLimit");
+        }
     }
 }
